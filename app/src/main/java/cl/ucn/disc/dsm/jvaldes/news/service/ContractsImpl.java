e898d87aafe06b4c2f0e2cb639238e4013b21bbf
JotapeValdesCondori <juan.valdes@alumnos.ucn.cl>
2020-11-23 15:54:32 -0300

    feat(ContractsImpl): add ContractsImpl class

---
+++ app/src/main/java/cl/ucn/disc/dsm/jvaldes/news/service/ContractsImpl.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 Juan Valdes Condori, juan.valdes@alumnos.ucn.cl
+ *
+ *                            Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ *                            The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *                            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.jvaldes.news.service;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import cl.ucn.disc.dsm.jvaldes.news.model.News;
+
+/**
+ * @author Juan Valdes
+ */
+
+public class ContractsImpl implements Contracts {
+    /**
+     * Get the list of News
+     *
+     * @param size size of the list.
+     * @return the List of News.
+     */
+    @Override
+    public List<News> retriveNews(final Integer size) {
+
+        //The list of news
+        final List<News> news = new ArrayList<>();
+        News prueba =  new News((long)123,"hola", "teletrece", "yo", "www.holi.com"
+                                ,"www.holi.com/imagen.jpg", "oikefvhjeroifheof", "wdfefgrtg"
+                                , ZonedDateTime.now());
+        for (int i = 0; i <5; i++){
+            news.add(prueba);
+        }
+
+
+        // TODO: Add the faker news to the list
+
+        return news;
+    }
+}
