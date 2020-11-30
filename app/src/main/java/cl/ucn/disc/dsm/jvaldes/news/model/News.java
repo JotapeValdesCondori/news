de957922c7f29cb13becf0d61b07aaa3be9fb3a3
JotapeValdesCondori <juan.valdes@alumnos.ucn.cl>
2020-11-30 19:50:42 -0300

    feat(news): Add validations.

--- app/src/main/java/cl/ucn/disc/dsm/jvaldes/news/model/News.java
+++ app/src/main/java/cl/ucn/disc/dsm/jvaldes/news/model/News.java
@@ -1,64 +1,75 @@
 /*
  * Copyright 2020 Juan Valdes Condori, juan.valdes@alumnos.ucn.cl
  *
- *                            Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- *                            The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- *                            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package cl.ucn.disc.dsm.jvaldes.news.model;
 
+import net.openhft.hashing.LongHashFunction;
+
 import java.time.ZonedDateTime;
+
+import cl.ucn.disc.dsm.jvaldes.news.utils.Validation;
 
 /**
  * The Domain model: News.
+ *
  * @author Juan Valdes.
  */
 
 public class News {
+
     /**
      * Unique id.
      */
-    private Long id;
+    private final Long id;
     /**
      * The title.
      * Restrictions: not null, size >2
      */
-    private String title;
+    private final String title;
+
     /**
      * The Source
      */
-    private String source;
+    private final String source;
+
     /**
      * The Author
      */
-    private String author;
+    private final String author;
+
     /**
      * The URL
      */
-    private String url;
+    private final String url;
+
     /**
      * The URL of Image
      */
-    private String urlImage;
+    private final String urlImage;
+
     /**
      * The Description
      */
-    private String desciption;
+    private final String desciption;
+
     /**
      * The Content
      */
-    private String content;
+    private final String content;
+
     /**
      * The Date of publish
      */
-    private ZonedDateTime PublishedAt;
+    private final ZonedDateTime publishedAt;
 
     /**
+     * The Constructor.
      *
-     * @param id
      * @param title
      * @param source
      * @param author
@@ -68,52 +79,95 @@ public class News {
      * @param content
      * @param publishedAt
      */
-    public News(Long id, String title, String source, String author, String url, String urlImage, String desciption, String content, ZonedDateTime publishedAt) {
-        this.id = id;
+    public News(String title, String source, String author, String url, String urlImage, String desciption, String content, ZonedDateTime publishedAt) {
+
+        // Validacion de titulo
+        Validation.minSize(title, 2, "title");
+
         this.title = title;
+
+        // Validacion de fuente
+        Validation.minSize(title, 2, "source");
+
         this.source = source;
+
+        // Validacion de autor
+        Validation.minSize(title, 2, "author");
+
         this.author = author;
+
+        this.id = LongHashFunction.xx().hashChars(title + "|" + source + "|" + author);
+
         this.url = url;
         this.urlImage = urlImage;
         this.desciption = desciption;
+
+        Validation.notNull(content, "content");
         this.content = content;
-        PublishedAt = publishedAt;
+        Validation.notNull(publishedAt, "publishedAt");
+        this.publishedAt = publishedAt;
     }
 
-
+    /**
+     * @return the id.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * @return the title.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * @return the source.
+     */
     public String getSource() {
         return source;
     }
 
+    /**
+     * @return the author.
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * @return the url.
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * @return the urlImage.
+     */
     public String getUrlImage() {
         return urlImage;
     }
 
+    /**
+     * @return the description.
+     */
     public String getDesciption() {
         return desciption;
     }
 
+    /**
+     * @return the content.
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * @return the publishedAt.
+     */
     public ZonedDateTime getPublishedAt() {
-        return PublishedAt;
+        return publishedAt;
     }
 }
