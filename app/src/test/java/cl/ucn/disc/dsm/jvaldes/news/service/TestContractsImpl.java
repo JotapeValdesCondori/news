a7179882cd364aa7719a498cec11f6ccceef0293
JotapeValdesCondori <juan.valdes@alumnos.ucn.cl>
2020-12-21 20:35:55 -0300

    feat(TestContractsImpl): edit TestContractsImpl class

--- app/src/test/java/cl/ucn/disc/dsm/jvaldes/news/service/TestContractsImpl.java
+++ app/src/test/java/cl/ucn/disc/dsm/jvaldes/news/service/TestContractsImpl.java
@@ -10,11 +10,15 @@ package cl.ucn.disc.dsm.jvaldes.news.service;
 
 import com.github.javafaker.Faker;
 
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 import cl.ucn.disc.dsm.jvaldes.news.model.News;
@@ -31,16 +35,79 @@ public class TestContractsImpl {
     public void testRetrieveNews (){
         log.debug("Testing...");
 
+        //The concrete implementation
         Contracts contracts = new ContractsImpl();
 
+        // Call the method..
         List <News> news = contracts.retriveNews(5);
+
+        // The list can´t be null
         Assertions.assertNotNull(news, "list was null :(");
+
+        // The list can´t be empty
         Assertions.assertTrue(news.size() != 0, "Empty list? :(");
+
+        // The size (list == 5)
         Assertions.assertTrue(news.size() == 5, "List Size != 5");
+
+        // debug to log
+        for (News n : news) {
+            log.debug("News: {}", ToStringBuilder.reflectionToString(n, ToStringStyle.MULTI_LINE_STYLE));
+        }
+
+        //size = 0
+        Assertions.assertEquals(0, contracts.retriveNews(0).size(),"List != 0");
+
+        //size = 3
+        Assertions.assertEquals(3, contracts.retriveNews(3).size(), "List != 3");
+
+        //size = 10
+        Assertions.assertTrue(contracts.retriveNews(10).size() <= 10, "List !=10");
 
         log.debug("Done.");
 
 
+
+
+    }
+
+
+    /**
+     * The Test of save news.
+     */
+    @Test
+    public void testSaveNews(){
+        log.debug("Testing..");
+
+        // The concrete implemetnation
+        Contracts contracts = new ContractsImpl():
+
+        // Nullity
+        Assertions.assertThrows(IllegalAccessException.class, () -> contracts.saveNews(null));
+
+        int size = contracts.retriveNews(1000).size();
+        log.debug("Size: {}.", size);
+
+        // Saving ok?
+        News news = new News(
+                "The Title",
+                "The Source",
+                "The Author",
+                null,
+                null,
+                "The Description",
+                "The Content",
+                ZonedDateTime.now(ZoneId.of("-3")));
+        contracts.saveNews(news);
+
+        // One more time!
+        int newSize = contracts.retriveNews(1000).size();
+        Assertions.assertEquals(size + 1, newSize, "Wrong size!");
+
+        // Save duplicated
+        Assertions.assertThrows(IllegalAccessException.class, () -> contracts.saveNews(news));
+
+        log.debug(".. Done.");
 
 
     }
